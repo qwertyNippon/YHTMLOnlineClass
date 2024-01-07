f2f7447d52d05731ef5b0f6b88dcf594b0c1250d
qwertyNippon <orlandohenry100@gmail.com>
2024-01-07 18:45:55 -0500

    left castle actions

--- port.js
+++ port.js
@@ -19,7 +19,7 @@
 let text = document.getElementById('text');
 let leaf = document.getElementById('leaf');
 let hill1 = document.getElementById('hill1');
-let hill4 = document.getElementById('hill4');
+let pic4 = document.getElementById('pic4');
 let hill5 = document.getElementById('hill5');
 
 window.addEventListener('scroll', () => {
@@ -29,7 +29,7 @@ window.addEventListener('scroll', () => {
     leaf.style.top = value * -1.5 + 'px';
     leaf.style.left = value * 1.5 + 'px';
     hill5.style.left = value * 1.5 + 'px';
-    hill4.style.left = value * -1.5 + 'px';
+    pic4.style.left = value * -1.5 + 'px';
     hill1.style.top = value * 1 + 'px';
 });
 
